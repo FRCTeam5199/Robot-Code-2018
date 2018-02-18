5be980f621e7ffb5c384db55e1cf499d6c53ea2e
HoboWithWifi <watomo00@gmail.com>
2018-02-18 14:47:12 -0800

    Added encoder

--- Test stuff/pancake code/src/elevator/Elevator.java
+++ Test stuff/pancake code/src/elevator/Elevator.java
@@ -1,33 +1,50 @@
 package elevator;
 
 import org.usfirst.frc.team5199.robot.RobotMap;
-import edu.wpi.first.wpilibj.Victor;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Elevator {
-	// 12 tooth 1.432" pitch diameter
-	// 15 tooth 1.790" pitch diameter
-	// 1024 ppr encoder?
-	private final double inchesPerPPR = 1.432d / 1024d;
-	//private TalonSRX motor;
-	private Victor motor;
-	
+	// 1-10 ratio
+	// 18 tooth 1.432 pitch diameter
+	// 1024 ppr encoder
+	private final double inchesPerPulse = (1.432d * 2) / (1024d * 10d);
+	private Spark motor;
+	private Encoder encoder;
+
+	private DigitalInput a;
+	private DigitalInput b;
+	private DigitalInput x;
 
 	public Elevator() {
-		//motor = new TalonSRX(RobotMap.elevatorMotor);
-		motor = new Victor(RobotMap.elevatorMotor);
+		// motor = new TalonSRX(RobotMap.elevatorMotor);
+		motor = new Spark(RobotMap.elevatorMotor);
+		// encoder = new Encoder(RobotMap.liftEncoderA, RobotMap.liftEncoderB,
+		// RobotMap.liftEncoderIndex);
+		encoder = new Encoder(RobotMap.liftEncoderA, RobotMap.liftEncoderB);
+		encoder.setDistancePerPulse(inchesPerPulse);
+		encoder.setDistancePerPulse(1);
+		//
+		// a = new DigitalInput(RobotMap.liftEncoderA);
+		// b = new DigitalInput(RobotMap.liftEncoderB);
+		// x = new DigitalInput(RobotMap.liftEncoderIndex);
 	}
 
 	public void setMotor(double d) {
-		//motor.set(com.ctre.phoenix.motorcontrol.ControlMode.PercentOutput, d);
+		// motor.set(com.ctre.phoenix.motorcontrol.ControlMode.PercentOutput,
+		// d);
 		motor.set(d);
 	}
 
-//	public double getLocation() {
-//		//return motor.getSensorCollection().getQuadraturePosition() * inchesPerPPR;
-//	}
-//
-//	public double getVelocity() {
-//		//return motor.getSensorCollection().getQuadratureVelocity() * inchesPerPPR;
-//	}
+	public void getPosition() {
+		// return encoder.getDistance();
+		// System.out.println(a.get() + " \t" + b.get() + " \t" + x.get());
+		System.out.println(encoder.getDistance());
+	}
+
+	public double getVelocity() {
+		return encoder.getRate();
+	}
 }
