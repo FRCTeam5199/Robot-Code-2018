445c5e36d13a65a127cea08961152a6d2e5898dc
HoboWithWifi <watomo00@gmail.com>
2018-02-18 14:49:14 -0800

    Updated robot map

--- Test stuff/pancake code/src/org/usfirst/frc/team5199/robot/RobotMap.java
+++ Test stuff/pancake code/src/org/usfirst/frc/team5199/robot/RobotMap.java
@@ -1,9 +1,15 @@
 package org.usfirst.frc.team5199.robot;
 
 public class RobotMap {
-	public static final int drivemotorLeft = 1;
-	public static final int drivemotorRight = 0;
+	public static final int mxpOffset = 10;
 	
-	public static final int elevatorMotor = 2;
+	public static final int drivemotorLeft = 8; // 1
+	public static final int drivemotorRight = 9; // 0
+
+	public static final int elevatorMotor = 7; // 2
+
+	public static final int liftEncoderA = 0 + mxpOffset;
+	public static final int liftEncoderB = 1 + mxpOffset;
+	public static final int liftEncoderIndex = 2 + mxpOffset;
 
 }
