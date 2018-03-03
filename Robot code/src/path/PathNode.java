e3523d75ecc55a62abc0111ad1ee987a55aa8ff5
HoboWithWifi <watomo00@gmail.com>
2018-03-03 14:37:59 -0800

    added path following code

---
+++ Robot code/src/path/PathNode.java
@@ -0,0 +1,33 @@
+package path;
+
+import maths.Vector2;
+
+public class PathNode {
+	private Vector2 position;
+	private double speed;
+
+	public PathNode(Vector2 position, double speed) {
+		this.position = position;
+		this.speed = speed;
+	}
+
+	public Vector2 getPos() {
+		return position;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setPos(Vector2 position) {
+		this.position = position;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	public String toString() {
+		return position + "\t" + speed;
+	}
+}
