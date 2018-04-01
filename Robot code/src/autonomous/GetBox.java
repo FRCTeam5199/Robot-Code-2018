962615f6d6e0de5a22064fb1d64a66b35cef1b26
HoboWithWifi <watomo00@gmail.com>
2018-03-31 22:53:35 -0700

    Changed GetBox distance stuff

--- Robot code/src/autonomous/GetBox.java
+++ Robot code/src/autonomous/GetBox.java
@@ -10,26 +10,26 @@ import interfaces.AutFunction;
 import maths.Vector2;
 import sensors.Location;
 
-public class GetBoxOld implements AutFunction {
+public class GetBox implements AutFunction {
 
 	private final double radToDeg = 180 / Math.PI;
-	private final double speed = .5;
+	private final double speed = 30;
 	private final int moveRange = 4;
-	private final int stopTurnRadius = 8;
-	private final int doneRadius = 6;
+	private final int doneRadius = 4;
+	private final int intakeDist = 26;
 
 	private final Location location;
 	private final DriveBase base;
 	private final DriveControl driveControl;
 	private final ElevatorControl elevatorControl;
 	private final Gripper gripper;
-	private final Vector2 pos;
+	private final Vector2 boxPos;
 
 	private double targetAngle;
 
-	public GetBoxOld(Vector2 pos, DriveBase base, DriveControl driveControl, ElevatorControl elevatorControl,
+	public GetBox(Vector2 boxPos, DriveBase base, DriveControl driveControl, ElevatorControl elevatorControl,
 			Gripper gripper) {
-		this.pos = pos;
+		this.boxPos = boxPos;
 		this.base = base;
 		this.driveControl = driveControl;
 		this.elevatorControl = elevatorControl;
@@ -43,10 +43,8 @@ public class GetBoxOld implements AutFunction {
 	public void update(long deltaTime) {
 		Vector2 robotPos = location.getLocation();
 		elevatorControl.setPositionSmooth(1, deltaTime);
-		if (Vector2.distance(location.getLocation(), pos) > stopTurnRadius) {
-			targetAngle = radToDeg * Math.atan2(pos.getX() - robotPos.getX(), pos.getY() - robotPos.getY());
-		}
 
+		targetAngle = radToDeg * Math.atan2(boxPos.getX() - robotPos.getX(), boxPos.getY() - robotPos.getY());
 		driveControl.setTurnPID(targetAngle);
 
 		double error = base.getGyro().getAngle() - targetAngle;
@@ -59,34 +57,46 @@ public class GetBoxOld implements AutFunction {
 		}
 
 		if (Math.abs(error) < moveRange) {
-			base.applyTurnPID(speed);
+			base.setPIDMove(speed);
 		} else {
-			base.applyTurnPID(0);
+			base.setPIDMove(0);
 		}
 
-		Robot.nBroadcaster.println(Vector2.distance(location.getLocation(), pos));
+		
+		base.applyPID();
+		
+		Robot.nBroadcaster.println(Vector2.distance(location.getLocation(), boxPos));
 
 	}
 
 	@Override
 	public void init() {
 		Vector2 robotPos = location.getLocation();
-		targetAngle = radToDeg * Math.atan2(pos.getX() - robotPos.getX(), pos.getY() - robotPos.getY());
+		targetAngle = radToDeg * Math.atan2(boxPos.getX() - robotPos.getX(), boxPos.getY() - robotPos.getY());
 		gripper.setSpeed(1);
 		gripper.open();
+		elevatorControl.resetSmoothStart();
 		elevatorControl.enablePID();
+		
+		driveControl.setTurnPIDDisplace();
+		driveControl.setMovePIDRate();
+		
 		driveControl.enableTurnPID();
+		driveControl.enableMovePID();
 	}
 
 	@Override
 	public boolean isDone() {
-		return Vector2.distance(location.getLocation(), pos) < doneRadius;
+		Vector2 intakePos = Vector2.rotateCW(new Vector2(0, intakeDist), location.getRot());
+		intakePos = Vector2.add(intakePos, location.getLocation());
+		return Vector2.distance(intakePos, boxPos) < doneRadius;
 	}
 
 	@Override
 	public void cleanUp() {
-		// TODO Auto-generated method stub
 		gripper.close();
+		driveControl.disableMovePID();
+		driveControl.disableTurnPID();
 		base.move(0, 0);
 		gripper.setSpeed(0);
 	}
